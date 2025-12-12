6ad1cdaa1c3a26493dbec161394819100a91eaad
shrutipathania0 <shrutipathania0@gmail.com>
2025-12-12 20:37:54 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Dashboard from "./pages/Dashboard";
+import Simulator from "./pages/Simulator";
+import Compare from "./pages/Compare";
+import Energy from "./pages/Energy";
+import Documentation from "./pages/Documentation";
+import History from "./pages/History";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/simulator" element={<Simulator />} />
+          <Route path="/compare" element={<Compare />} />
+          <Route path="/energy" element={<Energy />} />
+          <Route path="/docs" element={<Documentation />} />
+          <Route path="/history" element={<History />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
